255f250fced87cf9cbb1fc17a14b54f598fef554
Lukas Zmoginas <lukas.zmoginas@nfq.lt>
2020-01-15 00:39:07 +0200

    implementation of postgresql container

--- Vagrantfile
+++ Vagrantfile
@@ -20,6 +20,14 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
         vbox.customize ["modifyvm", :id, "--cpus", 4]
         vbox.customize ["modifyvm", :id, "--memory", "6144"]
         vbox.customize ["modifyvm", :id, "--name", $vm_name]
+        vbox.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+
+        vbox.customize ["modifyvm", :id, "--nic1", "nat"]
+        vbox.customize ["modifyvm", :id, "--nictype1", "virtio"]
+
+        vbox.customize ["modifyvm", :id, "--nic2", "hostonly"]
+        vbox.customize ["modifyvm", :id, "--nictype2", "virtio"]
+        vbox.customize ["modifyvm", :id, "--nicpromisc2", "allow-all"]
 
         vbox.customize ["setextradata", :id, "--VBoxInternal2/SharedFoldersEnableSymlinksCreate/v-root", "1"]
         vbox.customize ["guestproperty", "set", :id, "/VirtualBox/GuestAdd/VBoxService/--timesync-set-threshold", 500]
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,64 @@
 #!/usr/bin/env bash
 
-Update () {
-    echo "-- Update packages --"
-    sudo apt-get update
-    sudo apt-get upgrade
+PROJECT_NAME="$1"
+PROJECT_IP="$2"
+PROJECT_ROOT="/home/project/databases"
+PROJECT_TLD="test"
+
+echo " = = = "
+echo " PROJECT_NAME= ${PROJECT_NAME}"
+echo " PROJECT_ROOT= ${PROJECT_ROOT}"
+echo " = = = "
+
+function setup_dockerhub {
+	if [ ! -z "$VM_DOCKERHUB_LOGIN" ] && [ ! -z "$VM_DOCKERHUB_PASSWORD" ]; then
+		local HASH="$(echo "${VM_DOCKERHUB_LOGIN}:${VM_DOCKERHUB_PASSWORD}" | base64)"
+
+		sed -ri \
+			-e 's/^(VM_DOCKERHUB_LOGIN=).+$/\1""/' \
+			-e 's/^(VM_DOCKERHUB_PASSWORD=).+$/\1""/' \
+			-e '/^VM_DOCKERHUB_HASH.+$/d' \
+		/vagrant/vm.cfg
+
+		echo "VM_DOCKERHUB_HASH='$HASH'" >> /vagrant/vm.cfg
+	else
+		if [ ! -z "$VM_DOCKERHUB_HASH" ]; then
+			VM_DOCKERHUB_LOGIN="$(echo "$VM_DOCKERHUB_HASH" | base64 -d | cut -d ":" -f 1)"
+			VM_DOCKERHUB_PASSWORD="$(echo "$VM_DOCKERHUB_HASH" | base64 -d | cut -d ":" -f 2-)"
+		fi
+	fi
+
+	if [ ! -z "$VM_DOCKERHUB_LOGIN" ] && [ ! -z "$VM_DOCKERHUB_PASSWORD" ]; then
+		echo 'Found Docker hub login info, setting up ... '
+        if ! sudo -u project docker login -u "$VM_DOCKERHUB_LOGIN" -p "$VM_DOCKERHUB_PASSWORD" &> /dev/null; then
+            echo -e "\033[0;31mERROR: Login failed, skipping dockerhub setup!\033[0m"
+        fi
+	fi
 }
-Update
 
-echo "-- Just to quiet down some error messages --"
-IPADDR=$(/sbin/ifconfig eth0 | awk '/inet / { print $2 }' | sed 's/addr://')
-sed -i "s/^${IPADDR}.*//" /etc/hosts
-echo $IPADDR ubuntu.localhost >> /etc/hosts
+function save_config_variables {
+    if [ -f /vagrant/vm.cfg ]; then
+		echo 'saving vm.cfg into environment'
+        cat /vagrant/vm.cfg >> /etc/environment
+    fi
+}
 
-echo "-- Install tools and helpers --"
-sudo apt-get install -y --force-yes vim htop curl
+setup_dockerhub
+save_config_variables
+
+sudo -u project mkdir -p $PROJECT_ROOT
+
+echo 'Configure unison server'
+sed -i 's~UNISON_SERVER_PATH=~UNISON_SERVER_PATH=/home/project/databases~' /etc/unison/server.conf
+
+# make directories not that dark on dark background
+echo 'DIR 30;47' > /home/project/.dircolors
+chown project:project /home/project/.dircolors
+
+cd $PROJECT_ROOT
+
+cp -a /vagrant/databases ./
+rm -rf '/home/project/projects'
+
+echo -e "\n## Update local /etc/hosts ...\n"
+echo "${PROJECT_IP}            ${PROJECT_NAME}.${PROJECT_TLD}" | sudo tee -a /etc/hosts
