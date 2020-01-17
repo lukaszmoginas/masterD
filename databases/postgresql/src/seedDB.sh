97f9964d619c38fb8117d8be073e596f02255675
Lukas Zmoginas <lukas.zmoginas@nfq.lt>
2020-01-17 18:50:24 +0200

    seeding DBMS

--- databases/postgresql/src/seedDB.sh
+++ databases/postgresql/src/seedDB.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-if [ -d  data/restore.sql ]; then
+
+# update privileges for mock data
+cd /home/project/src || exit
+sudo chmod 0777 -R data/
+
+# add psql client to have ability to connect to the DBMS
+sudo apt-get install wget ca-certificates
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/lsb_release -cs-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get update -y -q
+sudo apt-get install postgresql-client -y -q
+
+FILE=/home/project/src/data/restore.sql
+
+if [ -f "$FILE"  ]; then
   createdb -h 10.24.1.17 -p 5432 -U postgres prod
   psql -h 10.24.1.17 -p 5432 -U postgres prod < data/restore.sql
 else
