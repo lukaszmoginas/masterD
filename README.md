cc8910aeaefbc43b1b09e772d022b71efa3268bf
Lukas Zmoginas <lukas.zmoginas@nfq.lt>
2020-01-19 12:53:09 +0200

    add postresql statistic table

--- README.md
+++ README.md
@@ -27,3 +27,21 @@ From vms:
 From host PC (1):
 ``` psql -h 10.24.1.17 -p 5432 -U postgres ```
 >  1 - (required postgresql client) ```sudo apt-get install postgresql-client```
+
+
+###NOTES:
+
+> Connect to DBMS and create extension in database; 
+>
+> Extension isn't installed:
+>
+>SELECT * 
+FROM pg_available_extensions 
+WHERE 
+    name = 'pg_stat_statements' and 
+    installed_version is not null;
+
+>If the table is empty, create the extension:
+>
+>CREATE EXTENSION pg_stat_statements;
+
--- databases/postgresql/src/seedDB.sh
+++ databases/postgresql/src/seedDB.sh
@@ -9,7 +9,10 @@ sudo apt-get install wget ca-certificates
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/lsb_release -cs-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo apt-get update -y -q
-sudo apt-get install postgresql-client -y -q
+sudo apt-get install postgresql-client postgresql-contrib -y -q
+sudo service postgresql restart
+
+#CREATE EXTENSION pg_stat_statements;
 
 FILE=/home/project/src/data/restore.sql
 
